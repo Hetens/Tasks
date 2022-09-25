110bc5ab9e5c6f90530c1ad467ed1c909fe636ee
Hetens <hetansh.shah95@gmail.com>
2022-09-25 12:27:44 +0530

    Task 3 Kinda works

--- Task3/queryform/views.py
+++ Task3/queryform/views.py
@@ -1,21 +1,38 @@
 
+from asyncio.windows_events import NULL
 from django.shortcuts import render
 import requests
 from .forms import Queryform
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 
+
+
+
+answer =[]
 # Create your views here.
-queries =['Which song is this']
 def index(request):
-    return render(request, 'queryform/index.html', {'queries':queries})
+    return render(request, 'queryform/index.html', {'queries':answer})
 
 def question(request):
+    url = "https://spotify23.p.rapidapi.com/search/"
+
+    
+
+    headers = {
+	"X-RapidAPI-Key": "3064e93a75msh4290adb8a28fe61p1a8456jsn4ac87731a7a1",
+	"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
+    }
+    
     if request.method =='POST':
         form =Queryform(request.POST)
         if form.is_valid():#cleaned_data is only invoked after the isvalid check
             query = form.cleaned_data["query"]
-            queries.append(query)
+            queries = query
+            querystring = {"q":queries,"type":"multi","offset":"0","limit":"10","numberOfTopResults":"5"}
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            r = response.json()
+            return render(request,'queryform/index.html',{'queries':r['tracks']['items']})
         else:
             return render(request, 'queryform/Form.html',{"form": form})#returns the existing form data
     return render(request, 'queryform/Form.html', {"form":Queryform()} )
--- Tryrequests.py
+++ Tryrequests.py
@@ -1,7 +1,15 @@
 
 import requests
 
+url = "https://spotify23.p.rapidapi.com/search/"
 
-response = requests.get('http://api.open-notify.org/astros.json',params = {'craft' :'ISS'})
+querystring = {"q":"Never gonna give you up","type":"multi","offset":"0","limit":"10","numberOfTopResults":"5"}
+
+headers = {
+	"X-RapidAPI-Key": "3064e93a75msh4290adb8a28fe61p1a8456jsn4ac87731a7a1",
+	"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.json())
